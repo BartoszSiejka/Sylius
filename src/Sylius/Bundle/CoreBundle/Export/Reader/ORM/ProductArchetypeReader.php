c3eb8b495b086d16f4685e9ae2833a907e2dae3e
BartoszSiejka <bartosz.siejka.svn@gmail.com>
2015-02-13 15:56:36 +0100

    Add archetype reader and writer

--- src/Sylius/Bundle/CoreBundle/Export/Reader/ORM/ProductArchetypeReader.php
+++ src/Sylius/Bundle/CoreBundle/Export/Reader/ORM/ProductArchetypeReader.php
@@ -11,139 +11,58 @@
 
 namespace Sylius\Bundle\CoreBundle\Export\Reader\ORM;
 
-use Monolog\Logger;
-use Sylius\Component\ImportExport\Model\JobInterface;
-use Sylius\Component\ImportExport\Reader\ReaderInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Sylius\Component\Core\Model\TaxonInterface;
 
 /**
- * Export product archetype reader.
+ * Export product attribute reader.
  *
  * @author Bartosz Siejka <bartosz.siejka@lakion.com>
  */
-class ProductArchetypeReader implements ReaderInterface {
-
+class ProductArchetypeReader extends AbstractDoctrineReader
+{
     private $productArchetypeRepository;
-    private $results;
-    private $configuration;
-    private $running = false;
-    private $logger;
     
-    /**
-     * @var int
-     */
-    private $resultCode = 0;
-    
-    /**
-     * Batch size
-     *
-     * @var integer
-     */
-    private $batchSize;
-
-    public function __construct(RepositoryInterface $productArchetypeRepository) 
+    public function __construct(RepositoryInterface $productArchetypeRepository)
     {
         $this->productArchetypeRepository = $productArchetypeRepository;
     }
-
-    public function read() 
+    
+    public function process($archetype)
     {
-        $taxons = array();
-       
-        if (!$this->running) {
-            $this->running = true;
-            $this->results = $this->getQuery()->execute();
-            $this->results = new \ArrayIterator($this->results);
-            $batchSize = $this->configuration['batch_size'];
-            $this->metadatas['row'] = 0;
-        }
+        $archetypes = array();
+        $options = $archetype->getOptions()->toArray();
+        $attributes = $archetype->getAttributes()->toArray();
+        $createdAt = (string) $archetype->getCreatedAt()->format('Y-m-d H:m:s');
         
-        for ($i = 0; $i < $batchSize; $i++) {
-            if ($result = $this->results->current()) {
-                $this->results->next();
-            }
-            $taxons = array_merge($taxons, $this->getChildren($result->getRoot()));
-        }
+        $attributeName = implode("~", $attributes);
+        $optionName = implode("~", $options);
         
-        foreach ($taxons as $taxon) {
-            $results[] = array(
-                'taxonomy_id'      => $taxon->getTaxonomy()->getId(),
-                'taxonomy_name'    => $taxon->getTaxonomy(),
-                'root_id'          => $taxon->getTaxonomy()->getRoot()->getId(),
-                'root_name'        => $taxon->getTaxonomy()->getRoot()->getName(),
-                'root_slug'        => $taxon->getTaxonomy()->getRoot()->getSlug(),
-                'root_permalink'   => $taxon->getTaxonomy()->getRoot()->getPermalink(),
-                'root_description' => $taxon->getTaxonomy()->getRoot()->getDescription(),
-                'root_left_tree'   => $taxon->getTaxonomy()->getRoot()->getLeft(),
-                'root_right_tree'  => $taxon->getTaxonomy()->getRoot()->getRight(),
-                'root_tree_level'  => $taxon->getTaxonomy()->getRoot()->getLevel(),
-                'id'               => $taxon->getId(),
-                'name'             => $taxon->getName(),
-                'slug'             => $taxon->getSlug(),
-                'permalink'        => $taxon->getPermalink(),
-                'description'      => $taxon->getDescription(),
-                'left_tree'        => $taxon->getLeft(),
-                'right_tree'       => $taxon->getRight(),
-                'tree_level'       => $taxon->getLevel(),
-                'parent_id'        => $taxon->getParent()->getId(),
-                'parent_name'      => $taxon->getParent()->getName(),
-            );
-        }
-        
-        $this->metadatas['row']++;
-           
-        return $results;
+        $archetypes = array_merge($archetypes, array(
+            'id'         => $archetype->getId(),
+            'code'       => $archetype->getCode(),
+            'name'       => $archetype->getName(),
+            'parent'     => $archetype->getParent(),
+            'options'    => $optionName,
+            'attributes' => $attributeName,
+            'created_at' => $createdAt,
+        ));
+         
+         return $archetypes;
     }
-
-    public function getChildren(TaxonInterface $taxon)
-    {
-        $children = $taxon->getChildren()->toArray();
-        
-        foreach ($children as $child) {
-            $children = array_merge($children, $this->getChildren($child));
-        }
-
-        return $children;  
-    }
-
-    public function getQuery() 
+    
+    public function getQuery()
     {
         $query = $this->productArchetypeRepository->createQueryBuilder('pac')
-                ->getQuery();
-
+            ->getQuery();
+        
         return $query;
     }
 
-    public function setConfiguration(array $configuration, Logger $logger) 
-    {
-        $this->configuration = $configuration;
-        $this->logger = $logger;
-    }
-    
     /**
      * {@inheritdoc}
      */
-    public function finalize(JobInterface $job)
-    {
-        $this->metadatas['result_code'] = $this->resultCode;
-        $job->addMetadata('reader',$this->metadatas);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getResultCode()
-    {
-        return $this->resultCode;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getType() 
+    public function getType()
     {
         return 'product_archetype';
     }
-
 }
